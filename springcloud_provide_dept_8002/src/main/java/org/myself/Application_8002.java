748b1a0c4e6d8348cbfd917bbe6e531728aab324
RainbowPerfect <903933616@qq.com>
2019-06-12 00:32:44 +0800

    添加工程

--- springcloud_provide_dept_8002/src/main/java/org/myself/Application_8002.java
+++ springcloud_provide_dept_8002/src/main/java/org/myself/Application_8002.java
@@ -1,13 +1,16 @@
 package org.myself;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class Application_Consumer_80 {
-    public static void main(String[] args) {
-        SpringApplication.run(Application_Consumer_80.class,args);
+public class Application_8002
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(Application_8002.class);
     }
 }
